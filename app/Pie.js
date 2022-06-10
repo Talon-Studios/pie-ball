250413496ebd8743096da54a12d49ccc0157d8c8
thcheetah777 <thcheetah777@gmail.com>
2022-06-10 14:23:23 -0700

    Powerball!

--- app/Pie.js
+++ app/Pie.js
@@ -11,12 +11,14 @@ CUSTARD
 export class Pie {
  constructor(_this, type) {
    this.pie = _this.pies.create(_this.player.x, _this.player.y, "pie");
+   this.pie.pieObj = this;
    this.pie.setScale(8);
    this.pie.setSize(6, 4);
    this.pie.setOffset(0, 0);
    this.pie.setGravityY(-1500);
    this.pie.setDepth(1);
-   this.pie.type = type;
-   _this.physics.velocityFromAngle(_this.player.pieAngle, _this.player.holdDur * 1.5, this.pie.body.velocity);
+   this.type = type;
+   this.power = (_this.player.holdDur / 5) / 2;
+   _this.physics.velocityFromAngle(_this.player.pieAngle, _this.player.holdDur, this.pie.body.velocity);
  }
 }
